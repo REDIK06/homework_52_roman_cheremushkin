8f654f66d23a615847d54300ff2d3dc8ed3e782c
roma <ceremuskinroma956@gmail.com>
2024-11-25 02:52:12 +0600

    Зарегистрировал модель task в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_completion']
+    list_filter = ['status']
+    search_fields = ['description']
